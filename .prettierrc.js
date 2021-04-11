0c3383e22bb011c09e721edcc875d75307988808
Saul H. Suarez Florez <saulhsf@gmail.com>
2021-04-11 17:58:25 -0500

    feat: init project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  useTabs: false,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true
+};
