609c63459360b879964ff32a250f08dfc2e72521
Saul H. Suarez Florez <saulhsf@gmail.com>
2021-04-11 19:02:00 -0500

    feat: config initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,8 +27,8 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+    //project: './tsconfig.json',
+    //tsconfigRootDir: __dirname,
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,35 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console']
+    }
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@src': ['./src/'],
+          '@assets': ['./src/assets/'],
+          '@configs': ['./src/configs/'],
+          '@constantsAPP': ['./src/configs/Constants.ts'],
+          '@styles': ['./src/styles/'],
+          '@helpers': ['./src/utils/helpers/'],
+          '@components': ['./src/components/'],
+          '@atoms': ['./src/components/atoms/'],
+          '@molecules': ['./src/components/molecules/'],
+          '@organisms': ['./src/components/organisms/']
+        }
+      }
+    ],
+    [
+      'babel-plugin-inline-import',
+      {
+        extensions: ['.svg']
+      }
+    ]
+  ]
 };
